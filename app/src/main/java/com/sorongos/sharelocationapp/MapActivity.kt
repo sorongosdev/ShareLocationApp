02931cd1a575e382ea3518f104cc900e049a187f
Sorongos <dosora9@gmail.com>
2023-03-28 15:58:54 +0900

    시드니에 마커 찍히게

--- app/src/main/java/com/sorongos/sharelocationapp/MapActivity.kt
+++ app/src/main/java/com/sorongos/sharelocationapp/MapActivity.kt
@@ -4,8 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.kakao.sdk.common.util.Utility
 import com.sorongos.sharelocationapp.databinding.ActivityMapBinding
 
@@ -18,15 +22,27 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        //callback이 구현된 mapActivity로 넘겨줌
+
         /**default debug keyHash*/
         var keyHash = Utility.getKeyHash(this)
         println(keyHash)
         Log.e("keyhash",keyHash.toString())
 
-        startActivity(Intent(this, LoginActivity::class.java))
+//        startActivity(Intent(this, LoginActivity::class.java))
     }
 
     override fun onMapReady(map: GoogleMap) {
         googleMap = map
+
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(
+            MarkerOptions()
+            .position(sydney)
+            .title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
