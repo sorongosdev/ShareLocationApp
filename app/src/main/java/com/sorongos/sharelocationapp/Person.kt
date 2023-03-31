64243be2adad661adc45b83c3744afb9128160c1
Sorongos <dosora9@gmail.com>
2023-03-31 15:55:45 +0900

    Create: data class

---
+++ app/src/main/java/com/sorongos/sharelocationapp/Person.kt
@@ -0,0 +1,9 @@
+package com.sorongos.sharelocationapp
+
+data class Person(
+    var uid: String? = null,
+    var latitude: Double? = null,
+    var longitude: Double? = null,
+    var name: String? = null,
+    var profilePhoto: String? = null,
+)
